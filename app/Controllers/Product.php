c73cdddcafc1e8344066b5428ce3b423a1a91cd0
AndiDemon <demon.yunus666@gmail.com>
2020-08-23 22:07:37 +0900

    frontend update

---
+++ app/Controllers/Product.php
@@ -0,0 +1,22 @@
+<?php namespace App\Controllers;
+
+class Product extends BaseController
+{
+	public function index()
+	{
+		//return view('welcome_message');
+		
+		echo view('frontend/header');
+		echo view('frontend/menu');
+		echo view('frontend/single-product');
+		//echo view('frontend/banner');
+		//echo view('frontend/carousel');
+		//echo view('frontend/trending');
+		//echo view('frontend/offer');
+		//echo view('frontend/best');
+		//echo view('frontend/blog-section');
+		//echo view('frontend/subscribe');
+		echo view('frontend/footer');
+		
+	}
+}
---
+++ app/Views/frontend/header.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Tsunagu - Share Easily, Get Freely</title>
+	<link rel="icon" href="<?php echo base_url('templates/aroma/img/Fevicon.png') ?>" type="image/png">
+  <link rel="stylesheet" href="<?php echo base_url('templates/aroma/vendors/bootstrap/bootstrap.min.css') ?>">
+  <link rel="stylesheet" href="<?php echo base_url('templates/aroma/vendors/fontawesome/css/all.min.css') ?>">
+	<link rel="stylesheet" href="<?php echo base_url('templates/aroma/vendors/themify-icons/themify-icons.css') ?>">
+  <link rel="stylesheet" href="<?php echo base_url('templates/aroma/vendors/nice-select/nice-select.css') ?>">
+
+    <link rel="stylesheet" href="<?php echo base_url('templates/aroma/vendors/linericon/style.css') ?>">
+  <link rel="stylesheet" href="<?php echo base_url('templates/aroma/vendors/owl-carousel/owl.theme.default.min.css') ?>">
+  <link rel="stylesheet" href="<?php echo base_url('templates/aroma/vendors/owl-carousel/owl.carousel.min.css') ?>vendors/owl-carousel/owl.carousel.min.css">
+
+  <link rel="stylesheet" href="<?php echo base_url('templates/aroma/css/style.css') ?>">
+</head>
+<body>
